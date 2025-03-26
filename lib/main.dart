a7cf5c7fd34e995e54da6026aa32241cd9292edf
Ruly Octareza <rulyoctareza@gmail.com>
2025-03-26 11:44:39 +0700

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     final themeController = Get.find<ThemeController>();
 
     return GetMaterialApp(
-      title: 'Portfolio',
+      title: 'Reza Dev Portofolio',
       debugShowCheckedModeBanner: false,
       themeMode: themeController.themeMode,
       theme: lightTheme,
